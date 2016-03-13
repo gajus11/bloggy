cf9275be56ee017927d01c683922fd3615b52f82
Łukasz Gajownik <lukasz@gajownik.com>
2016-03-13 22:26:27 +0100

    Dodanie widoku oraz templatek dla Post: list i detail.

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,13 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -22,8 +27,18 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    objects = models.Manager()  #default manager
+    published = PublishedManager()  #custom manager
+
     class Meta:
         ordering = ['-publish']
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
 
     def __str__(self):
         return self.title

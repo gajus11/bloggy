22a240409b2e74975c60b61dc5f6aee4d2b44732
Łukasz Gajownik <lukasz@gajownik.com>
2016-03-14 18:53:31 +0100

    Dodanie paginacji i szablonu css.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts' : posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 5) #5 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #if page is not an integer deliver first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        #if page is out of range deliver the last one
+        posts = paginator.page(paginator.num_pages)
+    context = {
+        'page' : page,
+        'posts' : posts,
+    }
+    return render(request, 'blog/post/list.html', context)
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,

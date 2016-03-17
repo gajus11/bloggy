6deb9ec81a5b15847ed188df9d1a3a053cad34ef
Łukasz Gajownik <lukasz@gajownik.com>
2016-03-17 20:49:26 +0100

    Dodana funkcjonalnosc: system komentarzy

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 # Create your views here.
@@ -14,23 +14,23 @@ class PostListView(ListView):
     paginate_by = 5
     template_name = 'blog/post/list.html'
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 5) #5 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        #if page is not an integer deliver first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        #if page is out of range deliver the last one
-        posts = paginator.page(paginator.num_pages)
-    context = {
-        'page' : page,
-        'posts' : posts,
-    }
-    return render(request, 'blog/post/list.html', context)
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 5) #5 posts in each page
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         #if page is not an integer deliver first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         #if page is out of range deliver the last one
+#         posts = paginator.page(paginator.num_pages)
+#     context = {
+#         'page' : page,
+#         'posts' : posts,
+#     }
+#     return render(request, 'blog/post/list.html', context)
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -39,7 +39,28 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post' : post})
+
+    #list of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        #a comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #create comment but don't save to db
+            new_comment = comment_form.save(commit=False)
+            #assign the current post to the comment
+            new_comment.post = post
+            #save comment to db
+            new_comment.save()
+    else:
+        comment_form = CommentForm
+    context = {
+        'post' : post,
+        'comments' : comments,
+        'comment_form' : comment_form,
+    }
+    return render(request, 'blog/post/detail.html', context)
 
 def post_share(request, post_id):
     #retrieve post by id
